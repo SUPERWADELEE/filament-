d6fcc7dddb8487d00ee70aff250f6c72f6b990ec
wade <elvis122545735@gmail.com>
2025-03-28 08:38:12 +0800

    Post comment 功能

---
+++ database/migrations/2025_03_26_103837_create_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            // 修改這行，使用 foreignId 並添加外鍵約束
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onDelete('cascade'); // 當用戶被刪除時，相關的文章也會被刪除
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
