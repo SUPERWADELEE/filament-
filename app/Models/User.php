73c8b2ad62d22ee9e1807293e4e379f97f2f06f8
李威德 <liweide@liweidedeMacBook-Air.local>
2025-04-13 23:00:24 +0800

    新增登入使用者權限設定

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Panel;
+use Filament\Models\Contracts\FilamentUser;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -50,6 +52,10 @@ class User extends Authenticatable
             'role' => 'string',
             'is_approved' => 'boolean',
         ];
+    }
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->role === 'doctor';
     }
 
     public function posts()
